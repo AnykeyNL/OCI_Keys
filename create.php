5dfa43e2cb9ce4351fcc654eb28adbdd9b8c5e8c
Richard Garsthagen <the.anykey@gmail.com>
2019-04-16 18:42:38 +0200

    Create create.php

---
+++ create.php
@@ -0,0 +1,32 @@
+<?php
+
+// Create SSH keys in both SSH and PEM format.
+//
+
+include('Crypt/RSA.php');
+require 'vendor/autoload.php';
+
+$opt = array(
+                'comment' => 'SSH Key files.',
+                'content_type' => 'application/octet-stream'
+        );
+
+$rsa = new Crypt_RSA();
+extract($rsa->createKey(2048));
+$rsa->loadKey($privatekey);
+$SSHprivatekey = $rsa->getPrivateKey(CRYPT_RSA_PRIVATE_FORMAT_PUTTY);
+$SSHpublickey = $rsa->getPublicKey(CRYPT_RSA_PUBLIC_FORMAT_OPENSSH);
+
+$zip = new ZipStream\ZipStream('OCI_Keys.zip', $opt);
+
+$info = "These are unique generated SSH key files. These files were created in memory only and saved to your local machine. Please save these files carefully, they can not be downloaded again!";
+
+$zip->addFile("API_private.pem", "$privatekey");
+$zip->addFile("API_public.pem", "$publickey");
+$zip->addFile("SSH_private.ppk", "$SSHprivatekey");
+$zip->addFile("SSH_public.pub", "$SSHpublickey");
+$zip->addFile("readme.txt", "$info");
+
+$zip->finish();
+
+?>
